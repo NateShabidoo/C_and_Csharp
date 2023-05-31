9392300928b28860e0728849eff414bce69be9a5
NateShabidoo <98194127+NateShabidoo@users.noreply.github.com>
2023-05-31 13:48:24 -0400

    Update ToDoList_App.cs

--- ToDoList_App.cs
+++ ToDoList_App.cs
@@ -1,4 +1,4 @@
-﻿
+
 var list = new List<string>() { "go to park", "run at gym" };
 
 
@@ -11,85 +11,87 @@ var list = new List<string>() { "go to park", "run at gym" };
 
     string userChoice = Console.ReadLine();
 
-    if (userChoice.ToUpper() == "S") {
-        
-        PrintSelectedOption("See all To Do's");
-        Console.WriteLine("Your to do list items are: ");
-        Console.WriteLine();
-        int i = 1;
-        foreach (string item in list) {
-            Console.WriteLine(i+". " +item);
-            i++;
-        }
-        Console.WriteLine();
-        Start();
-    }
-    else if (userChoice.ToUpper() == "A")
+    switch(userChoice)
     {
-        PrintSelectedOption("Add item to To Do list");
-        Console.WriteLine();
-        Console.WriteLine("please enter new item for to do list");
-        string newItem = Console.ReadLine();
-        list.Add(newItem);
-        Console.WriteLine("You have entered: "+newItem);
-        Console.WriteLine();
-        Console.WriteLine("Item added, updated list is: ");
-        foreach (string item in list)
-        {
-            Console.WriteLine(item);
+        case "S":
+        case "s":
+            PrintSelectedOption("See all To Do's");
+            Console.WriteLine("Your to do list items are: ");
             Console.WriteLine();
-        }
-        Start();
-    }
-    else if (userChoice.ToUpper() == "R")
-    {
-        PrintSelectedOption("Remove item from To Do list");
-        Console.WriteLine();
-        int i = 0;
-        int x = 1;
-        foreach (string item in list)
-        {
-            Console.WriteLine("Enter "+ x +" to remove "+"\""+list[i]+"\"");
-            i++;
-            x++;
+            int i = 1;
+            foreach (string item in list)
+            {
+                Console.WriteLine(i + ". " + item);
+                i++;
+            }
             Console.WriteLine();
-        }
-        Console.WriteLine("please enter your selection number");
-        Console.WriteLine();
-        string removeItem = Console.ReadLine();
-        int intRemoveItem = int.Parse(removeItem);
-        var nixedItem = list[intRemoveItem - 1];
-        list.Remove(list[intRemoveItem-1]);
-        Console.WriteLine("Item " + "\""+nixedItem+"\"" + " has been removed");
-        Console.WriteLine();
-        int y = 1;
-        Console.WriteLine("updated list is: ");
-        foreach (string item in list)
-        {
+            Start();
+            break;
+        case "A":
+        case "a":
+            PrintSelectedOption("Add item to To Do list");
             Console.WriteLine();
-            Console.WriteLine(y + ". " +item);
-            y++;
-            
-        }
-        Console.WriteLine();
-        Start();
+            Console.WriteLine("please enter new item for to do list");
+            string newItem = Console.ReadLine();
+            list.Add(newItem);
+            Console.WriteLine("You have entered: " + newItem);
+            Console.WriteLine();
+            Console.WriteLine("Item added, updated list is: ");
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+            Start();
+            break;
+        case "R":
+        case "r":
+            PrintSelectedOption("Remove item from To Do list");
+            Console.WriteLine();
+            int r = 0;
+            int x = 1;
+            foreach (string item in list)
+            {
+                Console.WriteLine("Enter " + x + " to remove " + "\"" + list[r] + "\"");
+                r++;
+                x++;
+                Console.WriteLine();
+            }
+            Console.WriteLine("please enter your selection number");
+            Console.WriteLine();
+            string removeItem = Console.ReadLine();
+            int intRemoveItem = int.Parse(removeItem);
+            var nixedItem = list[intRemoveItem - 1];
+            list.Remove(list[intRemoveItem - 1]);
+            Console.WriteLine("Item " + "\"" + nixedItem + "\"" + " has been removed");
+            Console.WriteLine();
+            int y = 1;
+            Console.WriteLine("updated list is: ");
+            foreach (string item in list)
+            {
+                Console.WriteLine();
+                Console.WriteLine(y + ". " + item);
+                y++;
+
+            }
+            Console.WriteLine();
+            Start();
+            break;
+        case "E":
+        case "e":
+            PrintSelectedOption("Exit");
+            break;
+        default:
+            PrintSelectedOption("Invalid Choice");
+            Start();
+            break;
     }
-    else if (userChoice.ToUpper() == "E")
-    {
-        PrintSelectedOption("Exit");
-    }
-    else
-    {
-        Console.WriteLine();
-        Console.WriteLine("Invalid Entry, Please Select Valid Entry");
-        Console.WriteLine();
-        Start();
-    }
-    
-}
+
 void PrintSelectedOption(string selectedOption) {
     Console.WriteLine("Selected Option: "+ selectedOption);
 }
+}
 Start();
 
-//Console.ReadKey();
+Console.WriteLine("press any key to exit");
+Console.ReadKey();
